81f3f33b5f9edff8b683dc0de52d9f7b1fccbd55
tanvirtbf <tanver018765@gmail.com>
2024-09-07 12:04:07 +0600

    change..........

--- Project 1 - Focus on today/script.js
+++ Project 1 - Focus on today/script.js
@@ -1,8 +1,20 @@
 const checkBoxList = document.querySelectorAll('.custom-checkbox')
+const inputList = document.querySelectorAll('.goal-input')
+const progressBar = document.querySelector('.progress-bar')
+
+const inputArr = [...inputList]
 
 checkBoxList.forEach((checkbox)=>{
   checkbox.addEventListener('click',(e)=>{
-    checkbox.parentElement.classList.toggle('completed');
+    const inputFilledOrNot = inputArr.every((i)=> {
+      return i.value;
+    })
+    if(inputFilledOrNot){
+      checkbox.parentElement.classList.toggle('completed');
+      errorLabel.style.display = 'none'
+    }else{
+      progressBar.classList.add('show-error')
+    }
   })
 })
 
