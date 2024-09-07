c0b3b92b2e935009e7a73a89c1c3b9c5de6ce8a6
tanvirtbf <tanver018765@gmail.com>
2024-09-07 14:01:12 +0600

    ui complete

--- Project 1 - Focus on today/script.js
+++ Project 1 - Focus on today/script.js
@@ -1,7 +1,7 @@
-const btn = document.querySelector('button')
-btn.addEventListener('click',()=>{
-  document.querySelector('.progress div').style.width = '60%'
-})
+// const btn = document.querySelector('button')
+// btn.addEventListener('click',()=>{
+//   document.querySelector('.progress div').style.width = '60%'
+// })
 
 
 
