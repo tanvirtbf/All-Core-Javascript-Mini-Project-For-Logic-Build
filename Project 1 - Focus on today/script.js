5b3e3b9fea99c3792fe35e1aa26821e8b2367443
tanvirtbf <tanver018765@gmail.com>
2024-09-07 14:25:32 +0600

    upate

--- Project 1 - Focus on today/script.js
+++ Project 1 - Focus on today/script.js
@@ -1,4 +1,33 @@
+const firstGoal = document.querySelector('#input1')
+const secondGoal = document.querySelector('#input2')
+const thirdGoal = document.querySelector('#input3')
 
+const circle = document.querySelectorAll('.circle')
 
+const inputObj = {
+  firstInput : '',
+  secondInput : '',
+  thirdInput : '',
+}
 
+firstGoal.addEventListener('input',(e)=>{
+  inputObj.firstInput = e.target.value
+})
+secondGoal.addEventListener('input',(e)=>{
+  inputObj.secondInput = e.target.value
+})
+thirdGoal.addEventListener('input',(e)=>{
+  inputObj.thirdInput = e.target.value
+})
+
+circle.forEach((item,i)=>{
+  console.log(i)
+  item.addEventListener('click',()=>{
+    if(inputObj.firstInput===''||inputObj.secondInput===''||inputObj.thirdInput===''){
+      document.querySelector('.error').style.display = 'block'
+    }else{
+      document.querySelector('.error').style.display = 'none'
+    }
+  })
+})
 
