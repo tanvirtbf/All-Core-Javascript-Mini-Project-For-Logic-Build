54ac2e68d13f2ba6eca6d61bf2ad604978644998
tanvirtbf <tanver018765@gmail.com>
2024-09-07 15:19:19 +0600

    update

--- Project 1 - Focus on today/script.js
+++ Project 1 - Focus on today/script.js
@@ -3,31 +3,55 @@ const secondGoal = document.querySelector('#input2')
 const thirdGoal = document.querySelector('#input3')
 
 const circle = document.querySelectorAll('.circle')
+const error = document.querySelector('.error')
 
-const inputObj = {
-  firstInput : '',
-  secondInput : '',
-  thirdInput : '',
-}
+// function setLocalStorage(obj){
+//   const stringConvert = JSON.stringify(obj)
+//   localStorage.setItem('focusObj',stringConvert)
+// }
+// function getLocalStorage(obj){
+//   const objectConvert = JSON.parse(localStorage.getItem('focusObj'))
+//   return objectConvert
+// }
+
+const inputArray = [
+  {id: 1, text : '', isActive : false},
+  {id: 2, text : '', isActive : false},
+  {id: 3, text : '', isActive : false},
+]
+
+// inputObj.firstInput = getLocalStorage(inputObj).firstInput
+// inputObj.secondInput = getLocalStorage(inputObj).secondInput
+// inputObj.thirdInput = getLocalStorage(inputObj).thirdInput
 
 firstGoal.addEventListener('input',(e)=>{
-  inputObj.firstInput = e.target.value
+  inputArray[0].text = e.target.value
 })
 secondGoal.addEventListener('input',(e)=>{
-  inputObj.secondInput = e.target.value
+  inputArray[1].text = e.target.value
 })
 thirdGoal.addEventListener('input',(e)=>{
-  inputObj.thirdInput = e.target.value
+  inputArray[2].text = e.target.value
 })
 
-circle.forEach((item,i)=>{
-  console.log(i)
+circle.forEach((item,index)=>{
+  console.log(item)
   item.addEventListener('click',()=>{
-    if(inputObj.firstInput===''||inputObj.secondInput===''||inputObj.thirdInput===''){
-      document.querySelector('.error').style.display = 'block'
+    if(inputArray[0].text===''||inputArray[1].text===''||inputArray[2].text===''){
+      error.style.display = 'block'
     }else{
-      document.querySelector('.error').style.display = 'none'
+      error.style.display = 'none'
+      inputArray.map((xyz)=> {
+        if(xyz.id===index+1){
+          xyz.isActive = !xyz.isActive
+        }
+      })
+      inputArray.map((abc)=>{
+        
+      })
     }
   })
 })
+
+
 
