fe47522d4003041f3c1747c007c5266da138da69
tanvirtbf <tanver018765@gmail.com>
2024-09-07 12:24:42 +0600

    transition testing complete

--- Project 1 - Focus on today/script.js
+++ Project 1 - Focus on today/script.js
@@ -1,21 +1,6 @@
-const checkBoxList = document.querySelectorAll('.custom-checkbox')
-const inputList = document.querySelectorAll('.goal-input')
-const progressBar = document.querySelector('.progress-bar')
-
-const inputArr = [...inputList]
-
-checkBoxList.forEach((checkbox)=>{
-  checkbox.addEventListener('click',(e)=>{
-    const inputFilledOrNot = inputArr.every((i)=> {
-      return i.value;
-    })
-    if(inputFilledOrNot){
-      checkbox.parentElement.classList.toggle('completed');
-      errorLabel.style.display = 'none'
-    }else{
-      progressBar.classList.add('show-error')
-    }
-  })
+const btn = document.querySelector('button')
+btn.addEventListener('click',()=>{
+  document.querySelector('.progress div').style.width = '60%'
 })
 
 
